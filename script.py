e46a9a42acf8ef9150c6fde63c0f24a11d0df94d
JeremyPie <91102181+JeremyPie@users.noreply.github.com>
2021-09-25 03:19:29 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,16 @@
 import cv2
-import time
-
-first_frame = None
+from datetime import time
 
 video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+first_frame = None
+
+status_list = []
+times = [0, 0]
 
 while True:
     check, frame = video.read()
+    status = 0
+    
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     gray_frame = cv2.GaussianBlur(gray_frame, (21,21), 0)
 
@@ -16,10 +20,27 @@ while True:
         
     delta_frame = cv2.absdiff(first_frame, gray_frame)
     thresh_frame = cv2.treshhold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    threhs_frame = cv2.dilate(thresh_frame, none, iteration = 2
     
+    (cnts, _) = cv2.findContours(thresh_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                              
+    for contour in cnts:
+        if cv2.contourArea(contour) >= 1000:
+            status += 1
+            (x,y,w,h) = cv2.boundRect(contour)
+            cv2.rectangle(frame, (x,y), (xw, yh), (0,255,0), 3)
+                              
+    status_list.append(status)
+    if status_list[-1] == 1 and status_list[-2] == 0: times.apend(datetime.now())
+    if status_list[-1] == 0 and status_list[-2] == 1: times.apend(datetime.now())
+
+                              
     cv2.imshow('Capturing...', gray_frame)
     key = cv2.waitKey(1)
-    if key == ord('q'): break
+    if key == ord('q'): 
+        if status == 1:
+            times.append(datetime.now())
+        break
 
 video.release()
 cv2.destroyAllWindows()
