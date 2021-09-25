75e6ebe7288766af9cf651e5027cb595d4282897
JeremyPie <91102181+JeremyPie@users.noreply.github.com>
2021-09-26 00:14:59 +0300

    Update testing_bokeh.py

--- exercises/testing_bokeh.py
+++ exercises/testing_bokeh.py
@@ -1,5 +1,6 @@
 from bokeh.plotting import figure
 from bokeh.io import output_file, show
+import pandas as pd
 
 x = [1,2,3,4,5]
 y = [6,7,8,9,10]
@@ -7,4 +8,14 @@ y = [6,7,8,9,10]
 output_file('line.html')
 f = figure()
 f.line(x,y)
+show(f)
+
+
+df = pd.read_csv('data.csv')
+x1 = df['x']
+y1 = df['y']
+
+output_file('line_from_csv.html')
+f = figure()
+f.line(x1,y1)
 show(f)
